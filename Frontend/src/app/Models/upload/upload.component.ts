37e395ca2d420bed286ffaa23e01f6e2a58461a7
DA_M1_44 <da_m1_44@Academy-learners-MacBook-Air-2.local>
2022-09-09 15:46:45 +0200

    working on upload file

--- Frontend/src/app/Models/upload/upload.component.ts
+++ Frontend/src/app/Models/upload/upload.component.ts
@@ -6,7 +6,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./upload.component.scss']
 })
 export class UploadComponent implements OnInit {
-  dob: Date = new Date();
 
   constructor() { }
 
