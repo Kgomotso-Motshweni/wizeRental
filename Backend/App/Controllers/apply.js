783aef2f4c293b474306ed3755ca08d79a89a797
DA_M1_44 <da_m1_44@Academy-learners-MacBook-Air-2.local>
2022-09-16 15:47:53 +0200

    to be continue

--- Backend/App/Controllers/apply.js
+++ Backend/App/Controllers/apply.js
@@ -1,10 +1,10 @@
 
-const pool = require("../config/config");
+const pool = require("../Config/db.config");
 
 const applyRoom = (request, response) => {
     const { fname, lname, email, phonenumber, age, uploadID, empStatus, income, viewDate, totOcc, petNum, petDesc, smoke} = request.body
   
-    pool.query('INSERT INTO item (fname, lname, email, phonenumber, age, uploadID, empStatus, income, viewDate, totOcc, petNum, petDesc, smoke) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13) RETURNING *', [fname, lname, email, phonenumber, age, uploadID, empStatus, income, viewDate, totOcc, petNum, petDesc, smoke], (error, results) => {
+    pool.query('INSERT INTO item (fname, lname, email, phonenumber, age, uploadID, empStatus, income, viewDate, totOcc, petNum, petDesc, smoke) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $3) RETURNING *', [fname, lname, email, phonenumber, age, uploadID, empStatus, income, viewDate, totOcc, petNum, petDesc, smoke], (error, results) => {
       if (error) {
         throw error
       }
@@ -16,4 +16,6 @@ const applyRoom = (request, response) => {
     applyRoom
     
   }
+
+  
 
