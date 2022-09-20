f4bc27b00423ff478b836919762f88c2819d6d91
Nomawethu <nomawethu.Khoza@dadevs.co.za>
2022-09-20 16:16:52 +0200

    getting property id

--- Frontend/src/app/Pages/landing/landing.component.ts
+++ Frontend/src/app/Pages/landing/landing.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {FormControl, FormGroup } from '@angular/forms';
 import { LandingService } from 'src/app/Services/landing.service';
 import { TenantService } from 'src/app/Services/tenant.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-landing',
@@ -12,11 +13,11 @@ export class LandingComponent implements OnInit {
  properties:any;
  property:any;
  searchItem:any;
-
+ condition: boolean = false;
  form!: FormGroup;
 
 //  private serv:LandingService
-  constructor(private service:LandingService) { }
+  constructor(private service:LandingService,private router: Router,private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     
@@ -35,16 +36,17 @@ export class LandingComponent implements OnInit {
         }
       })
 
-
-
   }
-
+  filter(){
+    this.condition = true
+  }
   // get details of the room
   accName:any;
   price:any;
   address:any;
   rooms:any;
   bedrooms:any;
+  id:any;
 
   
   // getProperty(numP: any) {
@@ -61,31 +63,21 @@ export class LandingComponent implements OnInit {
 
 
 
-//   onClick(p: any) {
-//     if(p == '20 - 30') {
-//        this.service.getProperties().subscribe(property => {
-//         this.property = property.filter(property => {
-//           return property.p_price >= 20 && property.p_price <= 30
-//         });
-//         console.log('PRODUCTS RANGE 20 -30', this.property);
-//       });
-//     } 
-//    if(p == '30 - 40') {
-//     this.service.getProperties().subscribe(property => {
-//       this.property = property.filter(property => {
-//         return property.p_price >= 20 && property.p_price <= 30
-//       });
-//       console.log('PRODUCTS RANGE 30 -40', this.property);
-//     });
-//     } if(p == '40 - 50') {
-//       this.service.getProperties().subscribe(property => {
-//         this.property = property.filter(property => {
-//           return property.p_price >= 20 && property.p_price <= 30
-//         });
-//         console.log('PRODUCTS RANGE 40 - 50', this.property);
-//       });
-//     } 
-// }
+onClick(ind:any){
+  this.service.getProperties().subscribe(
+    {
+      next: (data: any) => {
+        console.log(data[ind].property_id);
+        this.id =data[ind].property_id;
+        localStorage.setItem('prop_id','id');
+      }
+    });
+
+    // this.router.routeReuseStrategy.shouldReuseRoute = () => false;
+    // this.router.onSameUrlNavigation = 'reload';
+    // this.router.navigate(['/view'], { relativeTo: this.route });
+
+}
 
 
 }
