f72de733c447ac7147e800f5f200f98467e0531c
Excellent <excellent.mashengete@dadevs.co.za>
2022-09-24 09:26:55 +0200

    fixed addproperty for uploading image and pdf in one request

--- Backend/App/Middlewares/fileUpload.js
+++ Backend/App/Middlewares/fileUpload.js
@@ -6,7 +6,9 @@ module.exports = multer({
     storage: multer.diskStorage({}),
     fileFilter: (req, file, cb ) => {
         let ext = path.extname(file.originalname);
-       
+        if(ext == ".png" && ext == ".pdf" && ext == ".jpg" && ext == ".jpeg"){
+            cb({message: 'Unsupported file format'}, false)
+        }
         cb(null, true);
     },
 });
--- Backend/App/Routes/Landlord.js
+++ Backend/App/Routes/Landlord.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const upload  = require("../Middlewares/fileUpload");
+const upload  = require("../Middlewares/profileUpload");
+const cpUpload = upload.fields([{ name: 'house', maxCount: 1 }, { name: 'pdf', maxCount: 1 }])
 const router = express.Router();
 const landlord = require("../Controllers/addProperty");
 const get_rentees  = require('../Controllers/get_rentees');
@@ -7,7 +8,8 @@ const pending = require('../Controllers/pending')
 const middleware = require("../Middlewares/userauth");
 const get_property = require("../Controllers/get_property");
 
-router.post('/add_property/:userid', upload.single("pdf"), landlord.addProperty);
+
+router.post('/add_property/:userid', cpUpload, landlord.addProperty);
 router.post('/add_rooms/:property_id', upload.single("image"), landlord.addRoomImages);
 router.get('/getproperty/:userid', landlord.getMyProperties);
 router.delete('/deleteProperty/:property_id', landlord.deleteMyProperty);
