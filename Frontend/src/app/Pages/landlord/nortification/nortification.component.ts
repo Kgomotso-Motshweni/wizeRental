67e4d6b072ac36a84d6014fc77e479d637b26509
koketso <koketso.tinyane@dadevs.co.za>
2022-09-28 14:57:13 -0700

    need s

--- Frontend/src/app/Pages/landlord/nortification/nortification.component.ts
+++ Frontend/src/app/Pages/landlord/nortification/nortification.component.ts
@@ -12,23 +12,38 @@ export class NortificationComponent implements OnInit {
   @ViewChild('ngxLoading', { static: false })
   ngxLoadingComponent!: NgxLoadingComponent;
   showingTemplate = false;
+
   public ngxLoadingAnimationTypes = ngxLoadingAnimationTypes;
   public loading = false;
   constructor(private receive:NortificationsService) {
+    // createTask(){
+    //   const Task = { 
+    //    taskname: this.formCreate.value.taskname,
+    //    date: this.formCreate.value.date,
+    //    time:  this.formCreate.value.time,
+    //   userid:'3'
+    //   }
+    //   console.log (Task)
+    //   this.taskS.createTask(Task).subscribe(()=>{console.log(" Task created")})
+    //   }
+      
 
+    // createTask(body:any){
+    //   return this.http.put("http://localhost:8000/createT",body)
+    // }
+    
    }
 
    notification()
    {
-    this.receive.landlordReceive(1).subscribe((res)=>{
+    this.receive.landlordReceive(1).subscribe((responsetenant)=>{
+     this.response = responsetenant
 
     })
    }
-
+   response :any
   ngOnInit(): void {
   }
   
-
-
 
 }
