75bde40f02601d0c8889be2b1b5a38886c868b06
Evidence <evidence.chauke@dadevs.co.za>
2022-09-29 08:07:56 +0200

    removed consoles

--- Backend/App/Controllers/apply.js
+++ Backend/App/Controllers/apply.js
@@ -19,7 +19,6 @@ const applyRoom = async(req, res) => {
             });
 
         }else{
-            console.log(id_document)
             client.query(`INSERT INTO applicationform (tenant_id, property_id, full_name, email, phone_num, age,  id_doc, occupation, view_date, num_tenants, num_pets, ped_desc, smoke)
             VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13)`, 
             [tenant_id, property_id, full_name, email, phone_num, age, id_document.url, occupation, view_date, num_tenants, num_pets, ped_desc, smoke], (error, results) => {
@@ -37,23 +36,8 @@ const applyRoom = async(req, res) => {
             error: "Database error when applying for rental property", //Database connection error
         });
     } 
-  }
+}
 
-  module.exports = {
+module.exports = {
     applyRoom
-  }
-
-  
-
-/*
- client.query(`INSERT INTO applicationform (tenant_id, full_name, email, phone_num, age,  id_doc, occupation, num_tenants, num_pets, ped_desc, smoke)
-            VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)`, 
-            [tenant_id, full_name, email, phone_num, age, id_doc.url, occupation, num_tenants, num_pets, ped_desc, smoke], (error, results) => {
-               if(error){
-                   return res.status(400).json({
-                       message: "Unable to apply to this property"
-                   });
-               }
-               return res.status(200).send({ message: 'tenant application successfully'}); //Return a status 200 if there is no error
-           })
-           */
+}
