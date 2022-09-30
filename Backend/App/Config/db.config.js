8014c15007deb1d9f399df71024a90050a6bb093
Nomawethu <nomawethu.Khoza@dadevs.co.za>
2022-09-30 11:12:45 +0200

    connecting to backend

--- Backend/App/Config/db.config.js
+++ Backend/App/Config/db.config.js
@@ -1,35 +1,18 @@
 
-//local connection
 
-//const {Client} = require("pg");
-//const client= new Client(process.env.DB_URL)
-
-//Using MSQL
-// const mysql = require('mysql');
-// const client = new mysql.createConnection({
-//     host     : process.env.DB_Host, 
-//     user     : process.env.DB_User, 
-//     database : process.env.DB_Data, 
-//     password : process.env.DB_Pass,
-//     ssl:{
-        // rejectUnauthorized: false //allows external access to database when using nodejs
-//     }
-// })
-
-// module.exports = client
 
 
 //
-const Pool = require('pg').Pool
+// const Pool = require('pg').Pool
 // const pool = new Pool("postgres://fxarrraqzxyuak:6730a1bbc832d76d2f2da0d1069315dd4f78c0010298e71906dcda81738b5710@ec2-52-71-23-11.compute-1.amazonaws.com:5432/d2cjd0a8ddb2ks")
 
-const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'wizerentalz',
-  password: 'Letsdoit!',
-  port: 5432,
-})
+// const pool = new Pool({
+//   user: 'postgres',
+//   host: 'localhost',
+//   database: 'wizerentalz',
+//   password: 'Letsdoit!',
+//   port: 5432,
+// })
 
 /*
 const {Client} = require("pg");
@@ -40,4 +23,21 @@ const client = new Client({
     }
 });
 */
-module.exports = pool;
+// module.exports = pool;
+
+
+//local connection
+
+//const {Client} = require("pg");
+//const client= new Client(process.env.DB_URL)
+
+// hosted
+const {Client} = require("pg");
+const client = new Client({
+    connectionString: process.env.DB_Posgres,
+    ssl:{
+        rejectUnauthorized: false //allows external access to database when using nodejs
+    }
+});
+
+module.exports = client
--- Backend/server.js
+++ Backend/server.js
@@ -1,9 +1,74 @@
+// const express = require('express'); // import express library
+// const cors = require('cors'); //import cors module
+// const app = express(); //Initialize express
+// const bodyParser = require('body-parser');
+
+// const user = require('../Backend/App/Routes/property')
+
+// require('./App/Config/dotenv.config')
+
+// var corsOptions = {
+//     origin: ["*", "http://localhost:4200"],
+//     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+//     preflightContinue: false,
+//     optionsSuccessStatus: 200,
+//     credentials: true
+//   };// only allow the listerning addresses to connnect to the backend
+  
+// app.use(express.json());  // to support JSON-encoded
+// app.use(cors(corsOptions));
+// app.use(express.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+// app.use(
+//     bodyParser.urlencoded({
+//         extended: true
+//     })
+// );
+
+// //call our database connections file for postgre
+// const client = require('./App/Config/db.config')
+// client.connect((err) =>{ // Connect to the Database
+//     if (err) {
+//        console.log(err) //Return an error if unable to connnect to the database
+//       }
+//    else {
+//      console.log("Databased Connected"); //Database connection Successfuly
+//     }
+// });
+
+// const port = process.env.PORT || 8080; //create a listerning port number
+
+// app.use('/users',user)
+
+// app.get("/", (req, res) =>{
+//     res.status(200).send("Welcome to WizeRentalz server");
+// });
+
+// //calling routes
+// // require('./App/Routes/property.route')(app)
+
+// //Routes Calls
+// const auth = require("./App/Routes/Authentication");
+// const property = require('./App/Routes/Landlord')
+// const tenant = require('./App/Routes/tenant')
+// const nortify = require('./App/Routes/Nortifications')
+// // const user =require('../Backend/App/Routes/rentees')
+
+// app.use("/api", auth) //retrive authentication infor 
+// app.use("/api", property) //retrive Landlord infor 
+// app.use("/api", tenant) //retrive Tenant infor 
+// app.use("/api", nortify) //retrive Tenant infor 
+
+// app.listen(port, () =>{  
+//     console.log(`Server is running on port ${port}. http://localhost:${port}`) 
+//  })
+
 const express = require('express'); // import express library
 const cors = require('cors'); //import cors module
 const app = express(); //Initialize express
 const bodyParser = require('body-parser');
 
-const user = require('../Backend/App/Routes/property')
+// const user = require('../Backend/App/Routes/property')
 
 require('./App/Config/dotenv.config')
 
@@ -38,14 +103,11 @@ client.connect((err) =>{ // Connect to the Database
 
 const port = process.env.PORT || 8080; //create a listerning port number
 
-app.use('/users',user)
 
 app.get("/", (req, res) =>{
     res.status(200).send("Welcome to WizeRentalz server");
 });
 
-//calling routes
-// require('./App/Routes/property.route')(app)
 
 //Routes Calls
 const auth = require("./App/Routes/Authentication");
@@ -53,6 +115,8 @@ const property = require('./App/Routes/Landlord')
 const tenant = require('./App/Routes/tenant')
 const nortify = require('./App/Routes/Nortifications')
 // const user =require('../Backend/App/Routes/rentees')
+
+// app.use('/api',user)
 
 app.use("/api", auth) //retrive authentication infor 
 app.use("/api", property) //retrive Landlord infor 
@@ -62,4 +126,3 @@ app.use("/api", nortify) //retrive Tenant infor
 app.listen(port, () =>{  
     console.log(`Server is running on port ${port}. http://localhost:${port}`) 
  })
-
